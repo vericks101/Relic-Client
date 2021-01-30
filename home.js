ce780e65611534e7d32c312d3bbefbe6202b6d6a
Victor Erickson <50758632+vericks101@users.noreply.github.com>
2021-01-29 21:53:14 -0800

    Added additional content to main section.

--- home.js
+++ home.js
@@ -23,17 +23,65 @@ let home = `
 
     .header-section p {
         margin: 0px;
+        font-size: 25px;
+        font-weight: bold;
+    }
+
+    .about-section {
+        text-align: left;
+        width: 550px;
+        float: right;
+        padding: 15px 15px 0px 0px;
+    }
+
+    .about-content-section {
+        background-color: red;
+    }
+
+    .period-separator {
+        color: white;
+    }
+
+    .about-content-section p {
+        padding: 5px 5px 5px 5px;
+    }
+
+    .game-manage-buttons {
+        right: 15px;
+        bottom: 40px;
+        position: absolute;
     }
 </style>
 <div class="header">
     <div class="image-section">
-        <img alt="" src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" width="20" height="20">
+        <img alt="" src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" width="40" height="40">
         
         <div class="header-section">
-            <p>foobar</p>
-            <span class="badge bg-secondary">Primary</span>
-            <span class="badge bg-secondary">Secondary</span>
+            <p>Ekodex</p>
+            <span class="badge bg-secondary">2D</span>
+            <span class="badge bg-secondary">RPG</span>
+            <span class="badge bg-secondary">Sandbox</span>
+            <span class="badge bg-secondary">Single Player</span>
         </div>
     </div>
+</div>
+<div class="about-section">
+    <h3>About Ekodex</h3>
+    <img style="padding-bottom: 4px;" data-bs-toggle="tooltip" data-bs-placement="right" title="Tooltip on right" alt="" src="https://cdn.logo.com/hotlink-ok/logo-social-sq.png" width="20" height="20">
+    <span class="period-separator">.</span>
+    <span>Version 1.0.0</span>
+    <span class="period-separator">.</span>
+    <span>Released Dec. 26th 2017</span>
+    <div class="about-content-section">
+        <p>
+            The very solar system is at your fingertips as you fight for the survival of the human race, for fortune, and for glory. 
+            Delve deep into the unique planets throughout the solar system and fight various races of aliens and other menacing foes. 
+            Blending the elements of classic action RPG games with the freedom of randomly generated sandbox-style creativity, 
+            Unbound is a unique gaming experience where both the journey and the destination are as unique as the player experience.
+        </p>
+    </div>
+</div>
+<div class="game-manage-buttons">
+    <button type="button" class="btn btn-outline-light">Download</button>
 </div>
 `
