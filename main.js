0261e09b6b1cf3d47ebefcde11dbb3d075108a99
Victor Erickson <50758632+vericks101@users.noreply.github.com>
2021-02-04 21:50:00 -0800

    Adding basic functionality for managing game within client.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
 var path = require('path');
 const { menu } = require('./menu');
-// const { download } = require("electron-dl");
+const { download } = require("electron-dl");
 
 let mainWindow;
 
@@ -26,33 +26,6 @@ function createWindow () {
   mainWindow.once('ready-to-show', () => {
     autoUpdater.checkForUpdatesAndNotify();
   });
-
-  // var child = require('child_process').execFile;
-  // var executablePath = "C:\\Program Files (x86)\\Alien Invasion\\Alien Invasion x86.exe";
-  // child(executablePath, function(err, data) {
-  //   if(err){
-  //      console.error(err);
-  //      return;
-  //   }
- 
-  //   console.log(data.toString());
-  // });
-
-  // var fs = require('fs');
-  // var filepath = "C:\\Users\\veric\\Desktop\\test1.txt";
-
-  // if (fs.existsSync(filepath)) {
-  //     fs.unlink(filepath, (err) => {
-  //         if (err) {
-  //             console.log("An error ocurred updating the file" + err.message);
-  //             console.log(err);
-  //             return;
-  //         }
-  //         console.log("File succesfully deleted");
-  //     });
-  // } else {
-  //     console.log("This file doesn't exist, cannot delete");
-  // }
 }
 
 app.on('ready', () => {
@@ -86,15 +59,104 @@ ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
+ipcMain.on('initialize_game_manage_buttons', (event) => {
+  var fs = require('fs');
+  var filepath = "C:\\Users\\veric\\Desktop\\test\\Build\\";
+
+  if (fs.existsSync(filepath)) {
+    event.sender.send('initialize_game_manage_buttons', { exists: true });
+  } else {
+    event.sender.send('initialize_game_manage_buttons', { exists: false });
+  }
+});
+
+ipcMain.on('delete_game', (event) => {
+  var fs = require('fs');
+  var filepath = "C:\\Users\\veric\\Desktop\\test\\Build\\";
+
+  if (fs.existsSync(filepath)) {
+      fs.rmdir(filepath, { recursive: true }, (err) => {
+          if (err) {
+              console.log("An error ocurred updating the folder" + err.message);
+              console.log(err);
+              return;
+          }
+          console.log("Folder succesfully deleted");
+      });
+  } else {
+      console.log("This folder doesn't exist, cannot delete");
+  }
+});
+
+ipcMain.on('launch_game', (event) => {
+  var child = require('child_process').execFile;
+  var executablePath = "C:\\Users\\veric\\Desktop\\test\\Build\\Unbound.exe";
+  child(executablePath, function(err, data) {
+    if(err){
+       console.error(err);
+       return;
+    }
+ 
+    console.log(data.toString());
+  });
+});
+
 ipcMain.on('restart_app', () => {
     autoUpdater.quitAndInstall();
 });
 
-// ipcMain.on("download", (event, info) => {
-//   info.properties.onProgress = status => mainWindow.webContents.send("download progress", status);
-//   download(BrowserWindow.getFocusedWindow(), info.url, info.properties)
-//       .then(dl => mainWindow.webContents.send("download complete", dl.getSavePath()));
-// });
+ipcMain.on("download", (event, info) => {
+  info.properties.onProgress = status => mainWindow.webContents.send("download progress", status);
+  download(BrowserWindow.getFocusedWindow(), info.url, info.properties)
+      .then(dl => mainWindow.webContents.send("download complete", dl.getSavePath()));
+});
+
+ipcMain.on('decompress-files', (event) => {
+  var DecompressZip = require('decompress-zip');
+  var ZIP_FILE_PATH = "C:\\Users\\veric\\Desktop\\Build.zip";
+  var DESTINATION_PATH = "C:\\Users\\veric\\Desktop\\test";
+  var unzipper = new DecompressZip(ZIP_FILE_PATH);
+
+  unzipper.on('error', function (err) {
+    console.log('Caught an error', err);
+  });
+
+  unzipper.on('extract', function (log) {
+    console.log('Finished extracting', log);
+
+    var fs = require('fs');
+    var filepath = "C:\\Users\\veric\\Desktop\\Build.zip";
+
+    if (fs.existsSync(filepath)) {
+        fs.unlink(filepath, (err) => {
+            if (err) {
+                console.log("An error ocurred updating the file" + err.message);
+                console.log(err);
+                return;
+            }
+            console.log("File succesfully deleted");
+        });
+    } else {
+        console.log("This file doesn't exist, cannot delete");
+    }
+
+    event.sender.send('extracting-finished');
+  });
+
+  unzipper.on('progress', function (fileIndex, fileCount) {
+    console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
+    const currentProgress = Math.floor(((fileIndex + 1) / fileCount) * 100);
+    event.sender.send('extracting-progress', { currentProgress: currentProgress });
+  });
+
+  unzipper.extract({
+    path: DESTINATION_PATH
+  });
+});
+
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
+});
 
 autoUpdater.on('update-available', () => {
     mainWindow.webContents.send('update_available');
