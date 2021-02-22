20ebd74a0c796a7cdc6eac40c31e65b525e58ade
Victor Erickson <50758632+vericks101@users.noreply.github.com>
2021-02-21 20:26:02 -0800

    Fixed client auto update notification UI.

--- src/client_updater/client-updater.js
+++ src/client_updater/client-updater.js
@@ -1,7 +1,7 @@
 ipcRenderer.on('update_available', () => {
     ipcRenderer.removeAllListeners('update_available');
     message.innerText = 'A new update is available. Downloading now...';
-    notification.classList.remove('hidden');
+    updateNotification.classList.remove('hidden');
 });
 
 ipcRenderer.on('update_downloaded', () => {
@@ -10,5 +10,5 @@ ipcRenderer.on('update_downloaded', () => {
     ipcRenderer.removeAllListeners('update_downloaded');
     message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
     restartButton.classList.remove('hidden');
-    notification.classList.remove('hidden');
+    updateNotification.classList.remove('hidden');
 });
